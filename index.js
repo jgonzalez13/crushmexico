26ee454aa74723333a3b1fde865d882a991709f5
Emmanuel Gonzalez <jgonzalez13@ucol.mx>
2020-04-03 23:01:39 -0600

    fucntion play

---
+++ index.js
@@ -0,0 +1,9 @@
+const gatell = document.getElementById("gatell");
+
+function playQuedateEnCasa() {
+  const quedateEnCasa = document.getElementById("quedateEnCasa");
+
+  quedateEnCasa.play();
+}
+
+gatell.addEventListener("click", playQuedateEnCasa);

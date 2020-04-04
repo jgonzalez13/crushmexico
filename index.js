281c8a0a42297a1e659b9e68f75916f7ca45281d
Emmanuel Gonzalez <jgonzalez13@ucol.mx>
2020-04-03 23:50:19 -0600

    version 2

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const gatell = document.getElementById("gatell");
 
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 function playQuedateEnCasa() {
   const quedateEnCasa = document.getElementById("quedateEnCasa");
 
-  quedateEnCasa.play();
+  const oyepPapi = document.getElementById("oyePapi");
+
+  if (getRandomInt(1, 2) === 1) quedateEnCasa.play();
+  else oyepPapi.play();
 }
 
 gatell.addEventListener("click", playQuedateEnCasa);
